199635712027d985ddb2306afacbc446c23faa4a
zanjie1999 <zanjie1999@yeah.net>
2026-07-29 01:38:07 +0800

    修复android14+弹不出所有文件访问授权的问题

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.zyyme.einklauncher"
         minSdk = 14
         targetSdk = 36
-        versionCode = 43
-        versionName = "2.7"
+        versionCode = 44
+        versionName = "2.8"
         multiDexEnabled = true
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
