df6fcc5a0769481a7e55c15adba9293fade9b72d
Sparkle <zhmie@outlook.com>
2026-09-05 00:34:43 +0800

    换个字体

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.zyyme.einklauncher"
         minSdk = 14
         targetSdk = 36
-        versionCode = 47
-        versionName = "3.1"
+        versionCode = 48
+        versionName = "3.2"
         multiDexEnabled = true
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
